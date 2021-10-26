eefe2c8efdd5aa95aa7b742658952acd6003c470
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2021-10-26 14:18:35 -0600

    gatsby tutorial step 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => (
   <Layout pageTitle="Home">
     <p>Following the gastby tutorial</p>
+    <StaticImage 
+      alt='Cliff'
+      src="../images/space.jpeg" 
+    />
   </Layout>
 )
 export default IndexPage;
